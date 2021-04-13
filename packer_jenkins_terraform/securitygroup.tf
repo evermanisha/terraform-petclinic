0e1bb2ac68ace5d99e019c9766e8b8675c64336c
manishaj <manisha.jain@tesco.com>
2021-04-13 16:33:27 +0530

    export port 9966

--- packer_jenkins_terraform/securitygroup.tf
+++ packer_jenkins_terraform/securitygroup.tf
@@ -43,6 +43,12 @@ resource "aws_security_group" "app-securitygroup" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+      from_port   = 9966
+      to_port     = 9966
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   ingress {
     from_port   = 80
     to_port     = 80
